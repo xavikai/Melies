60240f0a366cce9156788e4e27f67ef3ab55c9f3
xavicaimel <xavikai@gmail.com>
2025-02-27 12:42:05 +0100

    Plantilla inicial
    UNITY VERSION: Unity 6 600.0.28f1
    ASSET: First person narrative adventures + complete puzzle engine
    
    Last Release Asset :  22/11/2024
    
    Version 1.1.2（current）
    
    Released: Nov 22, 2024
    
    1.1.4: Unity 6 package. Unity 2020 package removed.

---
+++ Assets/AP/Assets/Script/Save/F_PlayerPrefs.cs
@@ -0,0 +1,20 @@
+﻿// Descritption : F_PlayerPrefs : Set input Type : Find on gameObject InputManager
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_PlayerPrefs : MonoBehaviour {
+	public void SetInputType(int inputType) {
+		PlayerPrefs.SetInt ("InputsType", inputType);
+		ingameGlobalManager.instance.switchKeyboardJoystick ();
+	}
+
+   /* private void Start()
+    {
+		if (!PlayerPrefs.HasKey("InputsType"))
+        {
+			PlayerPrefs.SetInt("InputsType", 1);
+			ingameGlobalManager.instance.switchKeyboardJoystick();
+		}		
+	}*/
+}
